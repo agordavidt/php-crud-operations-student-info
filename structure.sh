0749c85053463748e288ebe9b449b3925efa13ac
David Agor <agordavid14@gmail.com>
2024-10-06 05:02:21 +0100

    project structure

---
+++ structure.sh
@@ -0,0 +1,9 @@
+php-crud-operations-student-info/
+│
+├── config.php         # Database connection
+├── index.php          # View all students and navigate to forms
+├── create.php         # Form to create new student
+├── edit.php           # Form to update existing student
+├── delete.php         # Delete student from database
+├── read.php           # Display all students
+└── styles.css         # CSS for styling the form
