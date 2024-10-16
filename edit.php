4fabfd9ec987fc71efb09d1210df5802deb3de6f
David Agor <agordavid14@gmail.com>
2024-10-16 15:44:11 +0100

    fetch table values to the edit form

--- edit.php
+++ edit.php
@@ -21,12 +21,12 @@ require "config.php";
 <div class="container mt-5">
     <?php
 
-    include("message.php");
+    include "message.php";
     // echo $_SESSION['message'];
 
     ?>
     <div class="row">
-        <div class="col-md-4 mx-auto">
+        <div class="col-md-6 mx-auto">
             <div class="card">
                 <div class="card-header">
                     <h4>Edit Student Record
@@ -43,37 +43,56 @@ require "config.php";
                     // echo $student_id;
                     
                     // always write the values in single quotes
-                   $query = "SELECT * FROM students WHERE student_id = '$stuent_id'";
+                   $query = "SELECT * FROM students WHERE student_id = '$student_id'";
+
+                   // execute query
+                   $query_run = mysqli_query($conn, $query);
+
+                   // check for records in database table
+                   if(mysqli_num_rows($query_run) > 0)
+                   {
+                        $student = mysqli_fetch_array($query_run);
+                        // print_r($student);
+
+                        ?>
+                        <form action="functions.php" method="POST">
+                            <div class="mb-3">
+                                <label for="">First Name</label>
+                                <input type="text" name="first_name" value="<?= $student['first_name']; ?>" class="form-control" required >
+                            </div>
+                            <div class="mb-3">
+                                <label for="">Last Name</label>
+                                <input type="text" name="last_name" value="<?= $student['last_name']; ?>" class="form-control" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="">Email</label>
+                                <input type="email" name="email" value="<?= $student['email']; ?>" class="form-control" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="">Phone</label>
+                                <input type="text" name="phone" class="form-control" value="<?= $student['phone']; ?>"  pattern="0{1}[0-9]{10}" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="">Course</label>
+                                <input type="text" name="course" class="form-control" value="<?= $student['course']; ?>"  required>
+                            </div>
+                            <div class="mb-3">
+                                <button type="submit" name="update_record" class="btn btn-primary">Update Record</button>
+                            </div>
+                        </form>
+
+
+                    <?php
+                   }
+                   else{
+                    echo "<h4>ID not found!</h4>";
+                   }
 
                 }
 
 
                 ?>
-                    <form action="functions.php" method="POST">
-                        <div class="mb-3">
-                            <label for="">First Name</label>
-                            <input type="text" name="first_name" class="form-control" required >
-                        </div>
-                        <div class="mb-3">
-                            <label for="">Last Name</label>
-                            <input type="text" name="last_name" class="form-control" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="">Email</label>
-                            <input type="email" name="email" class="form-control" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="">Phone</label>
-                            <input type="text" name="phone" class="form-control" pattern="0{1}[0-9]{10}" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="">Course</label>
-                            <input type="text" name="course" class="form-control" required>
-                        </div>
-                        <div class="mb-3">
-                            <button type="submit" name="update_record" class="btn btn-primary">Update Record</button>
-                        </div>
-                    </form>
+                    
                 </div>
             </div>
             
